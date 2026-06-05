dadf76475c1874c7f541923d42f9dcfeb56974ca
najmulmyself <najmul.myself@gmail.com>
2026-06-05 06:05:05 +0600

    remove unused PostgreSQL connection pool initialization

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,6 @@ import { authRoute } from "./modules/auth/auth.route";
 import {Pool} from "pg";
 const app = express();
 
-const pool = new Pool({
-    connectionString : "postgresql://neondb_owner:npg_O6uqRmvIP9np@ep-late-paper-aql1kb2l-pooler.c-8.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
-});
-
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/issues", issueRoute);
